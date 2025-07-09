99f16aaacfa48782c5d97c0c5fd7174fedbb182f
irving6600 <irving6600@gmail.com>
2025-07-09 13:06:48 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Birthstone, Open_Sans } from "next/font/google";
+import { Birthstone, Open_Sans } from "next/font/google";
 import "./globals.css";
 
 const openSans = Open_Sans({
